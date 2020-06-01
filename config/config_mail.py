af5455f42bc22491c6109b44165872bddd890333
qinyj12 <qinyj12@gmail.com>
2020-06-01 22:20:50 +0800

    remove config

--- config/config_mail.py
+++
@@ -1,8 +0,0 @@
-class Config(object):
-    def __init__(self):
-        self.DEBUG = True
-        self.MAIL_SERVER = 'smtp.qq.com'
-        self.MAIL_PORT = '465'
-        self.MAIL_USE_SSL = True
-        self.MAIL_USERNAME = '1562555013@qq.com'
-        self.MAIL_PASSWORD = 'udscmadqrrsofebf'
