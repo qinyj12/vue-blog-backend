e9729a85ffb27b800e3dc4e3aeab5ac9188a461c
qinyj12 <qinyj12@gmail.com>
2020-06-24 18:18:22 +0800

    backup

--- api/say_hello.py
+++ api/say_hello.py
@@ -1,5 +1,18 @@
-from flask import current_app as app
+# -*- coding: UTF-8 -*-
+# from flask import current_app as app
+from flask import make_response, Blueprint
 
-@app.route('/hello')
+app_hello = Blueprint('app_hello', __name__)
+@app_hello.route('/hello', methods = ['GET', 'POST'])
 def say_hello():
-	return 'Hello World!'
+    resp = make_response()
+    resp.data = 'Hello World!'
+    resp.msg = 'yesyesyes'
+    return resp
+
+
+# @app.route('/hello', methods = ['GET', 'POST'])
+# def say_hello():
+#     resp = make_response()
+#     resp.data = 'Hello World!'
+#     return resp
--- app.py
+++ app.py
@@ -2,23 +2,28 @@
 from flask import Flask
 from flask_cors import CORS
 from config import config_logger
+from api import say_hello
 
 app = Flask(__name__)
 # 引入flask的配置文件
 app.config.from_pyfile('./config/config_flask.py')
 # 引入logger的配置文件
 app.logger.addHandler(config_logger.initialize_logger())
+# 注册蓝图
+app.register_blueprint(say_hello.app_hello)
 
 CORS(app, supports_credentials=True)
 
 with app.app_context():
-    from api import (api_mailcode, 
-                    api_signup, 
-                    api_login, 
-                    api_reset_password, 
-                    api_error, 
-                    api_get_session, 
-                    api_logout,
-                    api_save_article,
-                    say_hello)
+    from api import (
+        api_mailcode, 
+        api_signup, 
+        api_login, 
+        api_reset_password, 
+        api_error, 
+        api_get_session, 
+        api_logout,
+        api_save_article,
+        # say_hello
+    )
     app.run(host = '0.0.0.0', port = 5000)
